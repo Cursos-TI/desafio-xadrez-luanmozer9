634f5d913d44b7b3014276d6c9b8667a3794e9e7
luanmozer9 <luanmozer9@gmail.com>
2025-10-07 19:42:38 +0000

    inclusao de loop

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(){
 
-    //torre: 5 casas direita, bispo: 5 casas diagonal cima e a direita, rainha: 8 casas esquerda 
-    int peca, i = 1;
-    
+    //torre: 5 casas direita, bispo: 5 casas diagonal cima e a direita, rainha: 8 casas esquerda, Cavalo: duas pra baixo e uma pra esquerda.
+    int peca, i = 1, movimento_completo = 1;
+        
         printf("===Escolha uma peça para movimentá-la===\n");
         printf("1.Torre\n");
         printf("2.Bipo\n");
         printf("3.Rainha\n");
+        printf("4.Cavalo\n");
         scanf("%d", &peca);
 
         switch (peca)
@@ -16,22 +17,38 @@ int main(){
             printf("Você selecionou TORRE\n");
             while (i<=5)
             {
-                printf("Esquerda\n");
+                printf("Direita\n");
                 i++;                
             }
             break;
         case 2:
-            printf("Você selecionou BISPO\n");
-            do
+            printf("Voce selecionou BISPO\n");
+            for (int i = 1; i == 1; i++)
             {
-                printf("Cima á Direita\n");
-                i++;
-            } while (i <= 5); 
+                for (int j = 1; j <= 5; j++)
+                {
+                    printf("Cima\n");
+                }
+                printf("Direita\n");                
+            }
+            
+           
             break;
         case 3:
             printf("Você selecionou RAINHA\n");
             for ( i; i <= 8; i++)
             {
+                printf("Esquerda\n");
+            }
+            break;
+        case 4:
+            printf("Você selecionaou CAVALO\n");
+            while (movimento_completo--)
+            {
+                for (int i = 1; i <= 2; i++)
+                {
+                    printf("Baixo\n");
+                }
                 printf("Esquerda\n");
             }
             break;
